b362437da272d2483b9db705ffd6568058099c6d
Eric Bridgeford <ebridge2@jhu.edu>
2016-12-11 22:42:18 -0500

    updated models

--- fngs/analyze/views.py
+++ fngs/analyze/views.py
@@ -6,8 +6,11 @@ from django.core.urlresolvers import reverse_lazy
 from .models import Dataset, Subject
 from .forms import DatasetForm, SubjectForm
 from ndmg.scripts.fngs_pipeline import fngs_pipeline
+from ndmg.scripts.ndmg_pipeline import ndmg_pipeline
 from django.conf import settings
 import time
+import importlib
+import imp
 from ndmg.utils import utils as mgu
 from threading import Thread
 from multiprocessing import Process
@@ -82,6 +85,12 @@ def create_subject(request, dataset_id):
 				'error_message': 'Brain File must be .nii or .nii.gz',
 			}
 			return render(request, 'analyze/create_subject.html', context)
+                
+                subject.dti_file = request.FILES['dti_file']
+                subject.mprage_file = request.FILES['mprage_file']
+                subject.bvals_file = request.FILES['bvals_file']
+                subject.bvecs_file = request.FILES['bvecs_file']
+                subject.labels_file = request.FILES['labels_file']
 		print subject.func_scan.url
 		subject.save()
 		return render(request, 'analyze/dataset.html', {'dataset': dataset})
@@ -90,19 +99,36 @@ def create_subject(request, dataset_id):
 	}
 	return render(request, 'analyze/create_subject.html', context)
 
-def analysis(dataset, sub_id, output_dir):
+def analysis(dataset_id, dataset, sub_id, output_dir):
 	subject = get_object_or_404(Subject, dataset=dataset, sub_id=sub_id)
 	subject.output_url = output_dir
+
+        ndmg_outdir = settings.OUTPUT_DIR + dataset_id + "/ndmgresults"
+        #ndmg_pipeline(subject.dti_file.url, subject.bvals_file.url, subject.bvecs_file.url, subject.mprage_file.url, settings.AT_FOLDER + '/atlas/MNI152_T1_2mm.nii.gz', settings.AT_FOLDER + '/mask/MNI152_T1_2mm_brain_mask.nii.gz', [settings.AT_FOLDER + '/label/desikan_2mm.nii.gz'], ndmg_outdir)
+
+        ndmg_run_cmd = "ndmg_pipeline " + str(subject.dti_file.url) + " " + str(subject.bvals_file.url) + " " + str(subject.bvecs_file.url) + " " + str(subject.mprage_file.url) + " " + str(settings.AT_FOLDER + '/atlas/MNI152_T1_2mm.nii.gz') + " " + str(settings.AT_FOLDER + '/mask/MNI152_T1_2mm_brain_mask.nii.gz') + " " + ndmg_outdir + " " + str(subject.labels_file.url)
+        mgu().execute_cmd(ndmg_run_cmd)
+
+        ndmg_bids = imp.load_source('ndmg_bids', '/ndmg/ndmg/scripts/ndmg_bids')
+        ndmg_bids.group_level(ndmg_outdir+"/graphs/", ndmg_outdir+"/qc", False)
+
+        #ndmg_bids_cmd = "ndmg_bids " + ndmg_outdir + "/graphs/ " + ndmg_outdir + "/qc group"
+        #mgu().execute_cmd(ndmg_bids_cmd)
+        
+
 	fngs_pipeline(subject.func_scan.url, subject.struct_scan.url, subject.an,
-			      settings.AT_FOLDER + '/atlas/MNI152_T1-2mm.nii.gz', settings.AT_FOLDER + '/atlas/MNI152_T1-2mm_brain.nii.gz',
-			      settings.AT_FOLDER + '/mask/MNI152_T1-2mm_brain_mask.nii.gz', settings.AT_FOLDER + '/mask/HarvOx_lv_thr25-2mm.nii.gz', 
-			      [settings.AT_FOLDER + '/label/desikan-2mm.nii.gz'], output_dir, stc=subject.slice_timing, fmt='graphml')
+                      settings.AT_FOLDER + '/atlas/MNI152_T1_2mm.nii.gz', settings.AT_FOLDER + '/atlas/MNI152_T1_2mm_brain.nii.gz',
+                      settings.AT_FOLDER + '/mask/MNI152_T1_2mm_brain_mask.nii.gz', settings.AT_FOLDER + '/mask/HarvOx_lv_thr25_2mm.nii.gz', 
+                      [settings.AT_FOLDER + '/label/desikan_2mm.nii.gz'], output_dir, stc=subject.slice_timing, fmt='graphml')
+
+
+
 	wd = os.getcwd()
 	# go to where the subject is
 	os.chdir(dataset.output_url)
 	sub_folder = re.split('/', subject.output_url)[-1]
 
-	mgu().execute_cmd('zip -r ' + str(sub_folder) + ".zip " + str(sub_folder))
+	mgu().execute_cmd('zip -r ' + str(sub_folder) + ".zip *")
 	# change directory back
 	os.chdir(wd)
 	# and update the subject
@@ -119,7 +145,7 @@ def analyze_subject(request, dataset_id, sub_id):
 		subject.save()
 		date = time.strftime("%d-%m-%Y")
 		output_dir = settings.OUTPUT_DIR + dataset_id + "/" + sub_id + "_" + date
-		p = Process(target=analysis, args=(dataset, sub_id, output_dir,))
+		p = Process(target=analysis, args=(dataset_id, dataset, sub_id, output_dir,))
 		p.daemon=True
 		p.start()
 	except:
@@ -150,6 +176,11 @@ def delete_subject(request, dataset_id, sub_id):
 	# clear all traces of the subject from our storage system
 	mgu().execute_cmd("rm -rf " + subject.func_scan.url)
 	mgu().execute_cmd("rm -rf " + subject.struct_scan.url)
+        mgu().execute_cmd("rm -rf " + subject.dti_file.url)
+        mgu().execute_cmd("rm -rf " + subject.mprage_file.url)
+        mgu().execute_cmd("rm -rf " + subject.bvals_file.url)
+        mgu().execute_cmd("rm -rf " + subject.bvecs_file.url)
+        mgu().execute_cmd("rm -rf " + subject.labels_file.url)
 	if subject.output_url is not None:
 		mgu().execute_cmd("rm -rf " + subject.output_url)
 	subject.delete()
