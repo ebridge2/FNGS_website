1f1622cc0ed83f221871a376f0b2b9bf90a1ef9c
Tanay Agarwal <tagarwa2@jhu.edu>
2017-04-25 20:17:36 -0400

    Update urls.py

--- fngs/analyze/urls.py
+++ fngs/analyze/urls.py
@@ -6,12 +6,5 @@ app_name = 'analyze'
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^(?P<dataset_id>[\w\-]+)/$', views.dataset, name='dataset'),
-	url(r'dataset/add/$', views.create_dataset, name='dataset-add'),
 	url(r'job/submit/$', views.submit_job, name='job-submit'),
-	url(r'dataset/(?P<dataset_id>[\w\-]+)/delete/$', views.delete_dataset, name='dataset-delete'),
-	url(r'dataset/(?P<dataset_id>[\w\-]+)/subject/add/$', views.create_subject, name='subject-add'),
-	url(r'dataset/(?P<dataset_id>[\w\-]+)/(?P<sub_id>[\w\-]+)/delete/$', views.delete_subject, name='subject-delete'),
-	url(r'dataset/(?P<dataset_id>[\w\-]+)/(?P<sub_id>[\w\-]+)/analyze/$', views.analyze_subject, name='subject-analyze'),
-	url(r'dataset/(?P<dataset_id>[\w\-]+)/(?P<sub_id>[\w\-]+)/results/$', views.get_results, name='subject-results'),
 ]
