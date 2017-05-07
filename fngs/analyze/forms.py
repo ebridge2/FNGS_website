298c13191f668a433416b0406f73be9d1bf65db5
Tanay Agarwal <tagarwa2@jhu.edu>
2017-05-07 15:23:23 -0400

    Update forms.py

--- fngs/analyze/forms.py
+++ fngs/analyze/forms.py
@@ -12,7 +12,7 @@ class SubmissionForm(forms.ModelForm):
 			'state':_('Analysis Level'),
 			'bucket':_('S3 Bucket Name'),
 			'bidsdir':_('S3 Bucket BIDS Directory'),
-			'jobdir':_('Job Directory'),
+			'jobdir':_('Unique Token'),
 			'creds_file':_('AWS Credentials File'),
 			'datasetname':_('Dataset Name'),
 			'modality':_('Modality'),
@@ -22,9 +22,9 @@ class SubmissionForm(forms.ModelForm):
 			'state':_('Level of analysis to perform'),
 			'bucket':_('Name of S3 bucket where data lives'),
 			'bidsdir':_('Path on S3 bucket where data lives'),
-			'jobdir':_('Directory of jobs to kill or check status'),
+			'jobdir':_('Unique identifier for job submission to facilitate later queries'),
 			'creds_file':_('File containing user credentials for AWS services'),
-			'datasetname':_('Dataset name'),
+			'datasetname':_('Dataset name (group analysis)'),
 			'modality':_('Modality of data'),
 			'slice_timing':_('The method in which slices were acquired.')
 		}
