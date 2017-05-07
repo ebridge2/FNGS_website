8683318e9efc7389ba537100c5b8edaca365ae5d
Tanay Agarwal <tagarwa2@jhu.edu>
2017-05-07 15:28:30 -0400

    Update forms.py

--- fngs/explore/forms.py
+++ fngs/explore/forms.py
@@ -7,24 +7,14 @@ from django.utils.translation import ugettext_lazy as _
 class QuerySubmissionForm(forms.ModelForm):
 	class Meta:
 		model = QuerySubmission
-		fields = ['state', 'bucket', 'bidsdir', 'jobdir', 'creds_file', 'datasetname', 'modality', 'slice_timing']
+		fields = ['state', 'jobdir', 'creds_file']
 		labels={
-			'state':_('Analysis Level'),
-			'bucket':_('S3 Bucket Name'),
-			'bidsdir':_('S3 Bucket BIDS Directory'),
-			'jobdir':_('Job Directory'),
-			'creds_file':_('AWS Credentials File'),
-			'datasetname':_('Dataset Name'),
-			'modality':_('Modality'),
-			'slice_timing':_('Slice Timing Method')
+			'state':_('Query type'),
+			'jobdir':_('Unique token'),
+			'creds_file':_('AWS Credentials File')
 		}
 		help_texts={
 			'state':_('Level of analysis to perform'),
-			'bucket':_('Name of S3 bucket where data lives'),
-			'bidsdir':_('Path on S3 bucket where data lives'),
-			'jobdir':_('Directory of jobs to kill or check status'),
-			'creds_file':_('File containing user credentials for AWS services'),
-			'datasetname':_('Dataset name'),
-			'modality':_('Modality of data'),
-			'slice_timing':_('The method in which slices were acquired.')
+			'jobdir':_('Unique token of jobs to kill or check status'),
+			'creds_file':_('File containing user credentials for AWS services')
 		}
