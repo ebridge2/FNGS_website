390fbecc5c39a26bf5e76b40d940accf093e61df
Tanay Agarwal <tagarwa2@jhu.edu>
2017-05-07 13:04:21 -0400

    Update urls.py

--- fngs/analyze/urls.py
+++ fngs/analyze/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'analyze'
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.submit_job, name='index'),
 	url(r'job/submit/$', views.submit_job, name='job-submit'),
 ]
