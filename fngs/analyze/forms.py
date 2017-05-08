27728527eb87664dc12719b81980a1ee08637483
Tanay Agarwal <tagarwa2@jhu.edu>
2017-05-08 00:18:33 -0400

    Update forms.py

--- fngs/analyze/forms.py
+++ fngs/analyze/forms.py
@@ -7,7 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 class SubmissionForm(forms.ModelForm):
 	class Meta:
 		model = Submission
-		fields = ['state', 'bucket', 'bidsdir', 'jobdir', 'creds_file', 'datasetname', 'modality', 'slice_timing']
+		fields = ['state', 'bucket', 'bidsdir', 'jobdir', 'creds_file', 'datasetname', 'modality', 'slice_timing', 'data_file']
 		labels={
 			'state':_('Analysis Level'),
 			'bucket':_('S3 Bucket Name'),
@@ -16,7 +16,8 @@ class SubmissionForm(forms.ModelForm):
 			'creds_file':_('AWS Credentials File'),
 			'datasetname':_('Dataset Name'),
 			'modality':_('Modality'),
-			'slice_timing':_('Slice Timing Method')
+			'slice_timing':_('Slice Timing Method'),
+			'data_file':_('Local Data (Zipped)')
 		}
 		help_texts={
 			'state':_('Level of analysis to perform'),
@@ -26,5 +27,6 @@ class SubmissionForm(forms.ModelForm):
 			'creds_file':_('File containing user credentials for AWS services'),
 			'datasetname':_('Dataset name (group analysis)'),
 			'modality':_('Modality of data'),
-			'slice_timing':_('The method in which slices were acquired.')
+			'slice_timing':_('The method in which slices were acquired.'),
+			'data_file':_('Local data to be uploaded to S3 bucket(stored in BIDS formatting and zipped).')
 		}
