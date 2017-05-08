d1b7cc7907302769a7e5bf06af43d7946c077a77
Tanay Agarwal <tagarwa2@jhu.edu>
2017-05-08 01:20:51 -0400

    Update forms.py

--- fngs/analyze/forms.py
+++ fngs/analyze/forms.py
@@ -7,7 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 class SubmissionForm(forms.ModelForm):
 	class Meta:
 		model = Submission
-		fields = ['state', 'bucket', 'bidsdir', 'jobdir', 'creds_file', 'datasetname', 'modality', 'slice_timing', 'data_file']
+		fields = ['state', 'bucket', 'bidsdir', 'jobdir', 'creds_file', 'datasetname', 'modality', 'slice_timing', 'data_file', 'upload_data_or_not']
 		labels={
 			'state':_('Analysis Level'),
 			'bucket':_('S3 Bucket Name'),
@@ -17,7 +17,8 @@ class SubmissionForm(forms.ModelForm):
 			'datasetname':_('Dataset Name'),
 			'modality':_('Modality'),
 			'slice_timing':_('Slice Timing Method'),
-			'data_file':_('Local Data (Zipped)')
+			'data_file':_('Local Data (Zipped)'),
+			'upload_data_or_not':_('Upload Local Data?')
 		}
 		help_texts={
 			'state':_('Level of analysis to perform'),
@@ -28,5 +29,6 @@ class SubmissionForm(forms.ModelForm):
 			'datasetname':_('Dataset name (group analysis)'),
 			'modality':_('Modality of data'),
 			'slice_timing':_('The method in which slices were acquired.'),
-			'data_file':_('Local data to be uploaded to S3 bucket(stored in BIDS formatting and zipped).')
+			'data_file':_('Local data to be uploaded to S3 bucket(stored in BIDS formatting and zipped).'),
+			'upload_data_or_not':_('Whether or not the local data should be uploaded to S3.')
 		}
